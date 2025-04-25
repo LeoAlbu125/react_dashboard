2993cd0a55b4859642376f81123609a6bbef7eea
leo desu <leonardoaall125@gmail.com>
2025-04-25 04:34:37 -0300

    new order of things

---
+++ layout/Header.tsx
@@ -0,0 +1,16 @@
+// components/Header.tsx
+import React from "react";
+
+interface HeaderProps {
+  title: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ title }) => {
+  return (
+    <header className="mb-6">
+      <h1 className="text-2xl font-bold text-gray-800">{title}</h1>
+    </header>
+  );
+};
+
+export default Header;
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.tsx
+import React from "react";
+import Sidebar from "../layout/Sidebar";
+import Header from "../layout/Header";
+import ExpenseTable from "./components/ExpenseTable";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar />
+      <div className="flex-1 p-4 overflow-y-auto">
+        <Header title="temporary"/>
+        <a>test</a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
